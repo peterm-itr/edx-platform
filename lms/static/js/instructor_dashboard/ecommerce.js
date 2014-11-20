633e3a59a0726510248b30d28968b2946b548081
Afzal Wali <afzal.wali@arbisoft.com>
2014-11-20 19:44:04 +0500

    Moved code to initialize and defined the default el.

--- lms/static/js/instructor_dashboard/ecommerce.js
+++ lms/static/js/instructor_dashboard/ecommerce.js
@@ -7,8 +7,16 @@ var edx = edx || {};
     edx.instructor_dashboard.ecommerce = {};
 
     edx.instructor_dashboard.ecommerce.AddExpiryCouponView = Backbone.View.extend({
+        el: 'li#add-coupon-modal-field-expiry',
         events: {
             'click input[type="checkbox"]': 'clicked'
+        },
+        initialize: function() {
+          _.bindAll(this, 'clicked');
+          $( "#coupon_expiration_date" ).datepicker({
+            minDate: 0
+          });
+          $('li#add-coupon-modal-field-expiry input[name="expiration_date"]').hide();
         },
         clicked: function (event) {
             if (event.currentTarget.checked) {
@@ -22,12 +30,6 @@ var edx = edx || {};
     });
 
     $(function() {
-        $( "#coupon_expiration_date" ).datepicker({
-             minDate: 0
-        });
-        $('li#add-coupon-modal-field-expiry input[name="expiration_date"]').hide();
-        var view = new edx.instructor_dashboard.ecommerce.AddExpiryCouponView({
-            el: $('li#add-coupon-modal-field-expiry')
-        });
+        var view = new edx.instructor_dashboard.ecommerce.AddExpiryCouponView();
     });
 }).call(this, Backbone, $, _);
